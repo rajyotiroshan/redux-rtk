f552b13b31f8e9f63b1e158c143519a9a1001aad
rajan <rajyotiroshan@gmail.com>
2023-06-14 22:09:33 +0530

    slice

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./store/index.js";
+import { store } from "./store";
 const rootEl = document.getElementById("root");
 const root = createRoot(rootEl);
 
