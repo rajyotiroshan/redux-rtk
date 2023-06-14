7944406d7f04fd7d5f8841015bd6e3300b647e3d
rajan <rajyotiroshan@gmail.com>
2023-06-14 17:03:05 +0530

    UsersList setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import UsersList from "./components/UsersList";
+
 function App() {
-  return <div className="text-blue-900 bg-blue pd-100 border-2 inline-block">App</div>;
+  return (
+    <div className="container mx-auto">
+      <UsersList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/UsersList.js
@@ -0,0 +1,5 @@
+function UsersList(props) {
+  return <div>UserList</div>;
+}
+
+export default UsersList;
