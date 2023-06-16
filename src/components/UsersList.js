f333886021c4064834ac18f4cb45ea3927320385
rajan <rajyotiroshan@gmail.com>
2023-06-16 21:24:56 +0530

    disptach thunk func in USerList

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
+
 function UsersList(props) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers);
+  }, []);
   return <div>UserList</div>;
 }
 
