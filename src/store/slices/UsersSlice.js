1c699b685b20a8361e4b45e0f279491a1f82954d
rajan <rajyotiroshan@gmail.com>
2023-06-17 12:59:10 +0530

    fetchUser thunk extraReducer

--- src/store/slices/UsersSlice.js
+++ src/store/slices/UsersSlice.js
@@ -1,13 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
 
 const userSlice = createSlice({
   name: "users",
   initialState: {
     data: [],
     isLoading: false,
-    error: null
+    error: null,
   },
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      //update state to show user loading
+      state.isLoading = true;
+    });
+
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      //update state with fetched data
+      state.isLoading = false;
+      state.data = action.payload; //[{},{}] list of users
+    });
+
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      //update state with error
+      state.isLoading = false;
+      state.error = action.error;
+    });
+  },
 });
 
 export const userReducer = userSlice.reducer;
