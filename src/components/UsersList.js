815355a5ad36a8218bdfb7d639e1fd24a516cf28
rajan <rajyotiroshan@gmail.com>
2023-06-17 21:10:26 +0530

    local UsersList loading and error state

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,30 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skelton from "./Skelton";
 
 function UsersList(props) {
+  const [isLoadingUser, setIsLoadingUser] = useState(false);
+  const [loadingUserError, setLoadingUserError] = useState(null);
+
   const dispatch = useDispatch();
 
-  const { isLoading, data, error } = useSelector((state) => {
+  const { data } = useSelector((state) => {
     return state.users; //{data:[], isLoading: false, error: null}
   });
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    setIsLoadingUser(true);
+    //dispatch returns a promise
+    dispatch(fetchUsers())
+      .unwrap() //gives brand new promise that follow general promise rule
+      .then(() => {
+        console.log("success");
+        setIsLoadingUser(false);
+        setLoadingUserError(false);
+      })
+      .catch(() => {
+        console.log("failed");
+        setLoadingUserError(true);
+        setIsLoadingUser(false);
+      });
+
+    //BAD
+    //setLoadingUserError(fasle) ;; wil vbe called immediately after dispatch
   }, [dispatch]);
 
   const handleUserAdd = () => {
     dispatch(addUser());
   };
 
-  if (isLoading) {
+  if (isLoadingUser) {
     //show Skelton
     return <Skelton times={6} className="h-10 w-full" />;
   }
 
-  if (error) {
+  if (loadingUserError) {
     return <div>Error fetching data...</div>;
   }
   const renderedUser = data.map((user) => {
