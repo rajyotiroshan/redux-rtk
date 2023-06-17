66fc919223dcc0ca2167fddfe2d1c1aaffdca42f
rajan <rajyotiroshan@gmail.com>
2023-06-17 12:59:43 +0530

    UserList.ja Skelton and loading

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,14 +1,29 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
+import Skelton from "./Skelton";
 
 function UsersList(props) {
   const dispatch = useDispatch();
 
+  const { isLoading, data, error } = useSelector((state) => {
+    return state.users; //{data:[], isLoading: false, error: null}
+  });
+
   useEffect(() => {
-    dispatch(fetchUsers);
-  }, []);
-  return <div>UserList</div>;
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  if (isLoading) {
+    //show Skelton
+    return <Skelton times={6} className="h-10 w-full" />;
+  }
+
+  if (error) {
+    return <div>Error fetching data...</div>;
+  }
+
+  return <div>{data.length}</div>;
 }
 
 export default UsersList;
