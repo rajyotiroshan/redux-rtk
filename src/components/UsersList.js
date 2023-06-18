68569960da8b421c20037b34ec777c26c89344c3
rajan <rajyotiroshan@gmail.com>
2023-06-18 18:48:25 +0530

    Button loading spinner for creating user

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -46,11 +46,11 @@ function UsersList(props) {
     <div>
       <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        {isCreatingUser ? (
-          "Creating User..."
-        ) : (
-          <Button onClick={handleUserAdd}>+ Add User</Button>
-        )}
+
+        <Button onClick={handleUserAdd} loading={isCreatingUser}>
+          + Add User
+        </Button>
+
         {creatingUserError && "Error Creating user..."}
       </div>
       {renderedUser}
