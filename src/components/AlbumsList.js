08c307ad388a5270a004bb95503054bec7e44ba8
rajan <rajyotiroshan@gmail.com>
2023-06-27 16:38:07 +0530

    basic AlbumsList struc

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,11 +1,33 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skelton from "./Skeleton";
+import Expandable from "./ExpandablePanel";
+import Button from "./Button";
+import ExpandablePanel from "./ExpandablePanel";
 
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-  console.log(data, error, isLoading);
-  
-  return <div>Albums for {user.name}</div>;
+  let content;
+  if (isLoading) {
+    content = <Skelton times={3} />;
+  } else if (error) {
+    content = <div> Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
