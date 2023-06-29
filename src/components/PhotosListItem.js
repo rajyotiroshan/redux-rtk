1c3af40a25f60efd3f22dbbc03176bcff241cd30
rajan <rajyotiroshan@gmail.com>
2023-06-29 18:01:11 +0530

    delete icon on hovr img

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,7 +1,14 @@
+import { GoTrashcan } from "react-icons/go";
+import { useRemovePhotoMutation } from "../store";
+
 function PhotosListItem({ photo }) {
   return (
-    <div className="relative m-2 ">
+    <div className="relative m-2 cursor-pointer m-2 ">
       <img src={photo.url} alt="random pic" className="h-20 w-20" />
+
+      <div className="absolute  inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
+        <GoTrashcan className="text-3xl" />
+      </div>
     </div>
   );
 }
