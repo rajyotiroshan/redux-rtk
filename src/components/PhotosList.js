5c1745d49898f12f8a84ac694857623da4365195
rajan <rajyotiroshan@gmail.com>
2023-06-29 17:51:34 +0530

    photo display

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,9 +1,44 @@
-import { useFetchPhotosQuery } from "../store";
+import { useFetchPhotosQuery, useAddPhotoMutation } from "../store";
+import Button from "./Button";
+import Skelton from "./Skeleton";
+import PhotosListItem from "./PhotosListItem";
 
 function PhotosList({ album }) {
-    const {results,error, isLoading} = useFetchPhotosQuery(album);
-  return <div>PhotosList</div>;
+  const { data, isFetching, error } = useFetchPhotosQuery(album);
+
+  const [addPhoto, addPhotoResult] = useAddPhotoMutation();
+
+  const handleAddPhoto = () => {
+    addPhoto(album);
+  };
+
+  let content;
+
+  if (isFetching) {
+    content = <Skelton times={4} className="h-8 w-8" />;
+  } else if (error) {
+    content = <div>Error fetching photos</div>;
+  } else {
+    content = data.map((photo) => {
+      return <PhotosListItem key={photo.id} photo={photo} />;
+    });
+  }
+  return (
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">
+          Photos In {album.title}
+          <Button loading={addPhotoResult.isLoading} onClick={handleAddPhoto}>
+            + Add Photo
+          </Button>
+        </h3>
+      </div>
+
+      <div className="mx-8 flex flex-row flex-wrap justify-center">
+        {content}
+      </div>
+    </div>
+  );
 }
 
 export default PhotosList;
-
--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,5 +1,9 @@
-function PhotosListItem() {
-  return <div>Photo</div>;
+function PhotosListItem({ photo }) {
+  return (
+    <div className="relative m-2 ">
+      <img src={photo.url} alt="random pic" className="h-20 w-20" />
+    </div>
+  );
 }
 
 export default PhotosListItem;
